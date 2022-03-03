6149cd0386f29aeaebf4aa6f5d09ed3750804ddb
yjglab <yjgdesign@gmail.com>
2022-03-04 02:21:02 +0900

    add get request 'login'

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 import express from "express";
 const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("root");
+};
+const handleLogin = (req, res) => {
+  return res.send("login");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 const handleListening = () => {
   console.log("===Listening OK");
 };
