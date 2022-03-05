7c3f343759a02ccda61ee67948bf6dcda0039098
yjglab <yjgdesign@gmail.com>
2022-03-05 23:11:37 +0900

    add router: global, user, video

--- src/server.js
+++ src/server.js
@@ -2,15 +2,28 @@ import express from "express";
 import morgan from "morgan";
 const app = express();
 const logger = morgan("dev");
-const handleHome = (req, res) => {
-  return res.send("root");
-};
-const handleLogin = (req, res) => {
-  return res.send("login");
-};
 app.use(logger);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const globalRouter = express.Router();
+const handleHome = (req, res) => {
+  return res.send("Root. Home");
+};
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => {
+  return res.send("Edit User");
+};
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => {
+  return res.send("Watch");
+};
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => {
   console.log("===Listening OK");
