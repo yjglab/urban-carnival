9b651f6121a780daf1b7a52b3bd9234fd13d80cb
yjglab <yjgdesign@gmail.com>
2022-03-05 22:20:48 +0900

    add morgan

--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 const app = express();
-
+const logger = morgan("dev");
 const handleHome = (req, res) => {
   return res.send("root");
 };
 const handleLogin = (req, res) => {
   return res.send("login");
-};
-const logger = (req, res, next) => {
-  console.log(`${req.url}`);
-  next();
 };
 app.use(logger);
 app.get("/", handleHome);
