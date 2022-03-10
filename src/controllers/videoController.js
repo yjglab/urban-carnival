a42bc748dca406b5fdaca4a8508f3f4f80d82e06
yjglab <yjgdesign@gmail.com>
2022-03-11 00:22:30 +0900

    partition base and other router files

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,10 +2,10 @@ export const trending = (req, res) => {
   res.render("home");
 };
 export const see = (req, res) => {
-  res.send("Watch video");
+  res.render("watch");
 };
 export const edit = (req, res) => {
-  res.send("Edit video");
+  res.render("edit");
 };
 export const search = (req, res) => {
   res.send("Search");
