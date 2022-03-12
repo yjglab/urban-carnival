c4f3f4a60651e472ac00fa5e8c9a4e0f5ad8342f
yjglab <yjgdesign@gmail.com>
2022-03-13 01:22:18 +0900

    mixin test

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,35 @@
+let testData = [
+  {
+    title: "test_1",
+    views: 11,
+    id: 1,
+    comments: 111,
+    createdAt: "1 days ago",
+  },
+  {
+    title: "test_2",
+    views: 22,
+    id: 2,
+    comments: 222,
+    createdAt: "2 days ago",
+  },
+  {
+    title: "test_3",
+    views: 33,
+    id: 3,
+    comments: 333,
+    createdAt: "3 days ago",
+  },
+  {
+    title: "test_4",
+    views: 44,
+    id: 4,
+    comments: 4,
+    createdAt: "4 days ago",
+  },
+];
 export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home" });
+  res.render("home", { pageTitle: "Home", testData });
 };
 export const see = (req, res) => {
   res.render("watch");
